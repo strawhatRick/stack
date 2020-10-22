582d492d62394dd031930c24432b1e3bad71403c
strawhatRick <34947117+strawhatRick@users.noreply.github.com>
2020-10-22 21:18:52 +0530

    updated with axios http requests

--- script.js
+++ script.js
@@ -4,21 +4,56 @@ let popElement = document.querySelector('.pop');
 let displayStack = document.querySelector('.stack');
 let stack = [];
 
-function pushElementToStack(){
+
+
+
+//display stack
+
+window.addEventListener('DOMContentLoaded', () => {
+    axios.get("https://crudcrud.com/api/f38e531735fd400dbe378bf437e8146b/stack/")
+    .then(response => {
+        let array = response.data[response.data.length - 1].stack;
+        //console.log(array);
+        for (let i in array){
+            let ele = document.createElement('h2');
+            ele.innerHTML = array[i];
+            displayStack.append(ele);
+        }
+    })
+});
+
+
+//push elements
+let pushElementToStack = function (){
     let i = inputData.value;
     if (i != ""){
+        
         stack.push(i);
-        ele = document.createElement('h2');
+        axios.post("https://crudcrud.com/api/f38e531735fd400dbe378bf437e8146b/stack/",{
+            stack
+        })
+        
+        
+        let ele = document.createElement('h2');
         ele.innerHTML = i;
         displayStack.append(ele);
     }
 }
+pushElement.addEventListener('click', pushElementToStack);
 
-function popElementFromStack(){
+
+//pop elements
+let popElementFromStack = function (){
     let i = stack[stack.length - 1];
     //console.log(i);
     if (i != "" && (stack.length > 0)){
+        
         stack.pop();
+        axios.post("https://crudcrud.com/api/f38e531735fd400dbe378bf437e8146b/stack/",{
+            stack
+        })
+        
         displayStack.lastElementChild.remove();
     }
 }
+popElement.addEventListener('click', popElementFromStack);
