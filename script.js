348bb16e9e9f3d4ebbf19268f3b312fae867ac91
strawhatRick <34947117+strawhatRick@users.noreply.github.com>
2020-10-22 22:23:58 +0530

    updated and working properly right now

--- script.js
+++ script.js
@@ -4,16 +4,12 @@ let popElement = document.querySelector('.pop');
 let displayStack = document.querySelector('.stack');
 let stack = [];
 
-
-
-
 //display stack
-
 window.addEventListener('DOMContentLoaded', () => {
     axios.get("https://crudcrud.com/api/f38e531735fd400dbe378bf437e8146b/stack/")
     .then(response => {
         let array = response.data[response.data.length - 1].stack;
-        //console.log(array);
+        stack = array;//assigning stack to array
         for (let i in array){
             let ele = document.createElement('h2');
             ele.innerHTML = array[i];
@@ -22,25 +18,20 @@ window.addEventListener('DOMContentLoaded', () => {
     })
 });
 
-
 //push elements
 let pushElementToStack = function (){
     let i = inputData.value;
     if (i != ""){
-        
         stack.push(i);
         axios.post("https://crudcrud.com/api/f38e531735fd400dbe378bf437e8146b/stack/",{
             stack
-        })
-        
-        
-        let ele = document.createElement('h2');
-        ele.innerHTML = i;
-        displayStack.append(ele);
+        })    
+    let ele = document.createElement('h2');
+    ele.innerHTML = i;
+    displayStack.append(ele);
     }
 }
 pushElement.addEventListener('click', pushElementToStack);
-
 
 //pop elements
 let popElementFromStack = function (){
@@ -49,6 +40,7 @@ let popElementFromStack = function (){
     if (i != "" && (stack.length > 0)){
         
         stack.pop();
+        //console.log(stack);
         axios.post("https://crudcrud.com/api/f38e531735fd400dbe378bf437e8146b/stack/",{
             stack
         })
